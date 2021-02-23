3aeb2a921918016f7a7c13c7482e239f61a0075e
yj6477 <youngjin6477@gmail.com>
2021-02-23 12:57:56 -0800

    edited config file and added selenium directory

---
+++ selenium/htmlUnitTest.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;	
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Assert;
+
+public class htmlUnitTest {				
+
+	public static void main(String[] args) {
+
+		// Creating a new instance of the HTML unit driver
+		WebDriver driver = new HtmlUnitDriver(true); //enable JavaScript
+		              
+		// Navigate to local Jekyll site
+		driver.get("http://localhost:4000");					
+          
+		// This code will test and print the page title
+		String pageTitle = driver.getTitle();
+		Assert.assertNotEquals(pageTitle, "");
+		Assert.assertFalse(pageTitle.matches("^\\s*$")); // uses regexp
+		System.out.println("Page title is: " + pageTitle);
+
+		// This code will check the HTML source for Jekyll 404 errors
+		String pageSource = driver.getPageSource();
+		Assert.assertFalse(pageSource.contains("404.html")); 
+		Assert.assertFalse(pageSource.contains("requested page could not be found")); 
+
+		// Create a test that is specific to YOUR Jekyll site here -->
+		String gitLink;
+		gitLink = driver.findElement(By.cssSelector(".username:first-of-type")).getText();
+		//if it's not my link occur error
+		Assert.assertEquals(gitLink,"yj6477");
+		System.out.println(gitLink);
+		     
+		// <-- End of your test code
+
+		driver.quit();			
+	}		
+}
